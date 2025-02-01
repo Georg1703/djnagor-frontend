aaeeb0bad6fb66bcffa084f5dc50d633cc9aaab0
Gheorghe Mirca <giony.mirca@gmail.com>
2025-02-01 06:21:21 +0200

    first commit

---
+++ src/components/ItemList.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const ItemList = () => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(import.meta.env.VITE_ADMIN_API_URL + "api/items/")
+      .then((response) => setItems(response.data))
+      .catch((error) => console.error(error));
+  }, []);
+
+  return (
+    <div>
+      <h1>Items List</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.name}: {item.description}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ItemList;
